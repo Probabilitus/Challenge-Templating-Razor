c5967a1a295300e5fdd5d6462cd412582b4d51c6
Diamantine <meteoritus75@gamil.com>
2024-06-18 18:09:41 +0200

    Challenge Templating Razor

---
+++ Controllers/ExpenditureController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using Templating_Razor.Models;
+
+namespace Templating_Razor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public ActionResult Index()
+        {
+            var expenditures = new List<Expenditure>
+        {
+            new Expenditure { Name = "Dépense 1", Date = DateTime.Now.AddDays(-1), Price = 10.0 },
+            new Expenditure { Name = "Dépense 2", Date = DateTime.Now.AddDays(-2), Price = 20.0 },
+            new Expenditure { Name = "Dépense 3", Date = DateTime.Now.AddDays(-3), Price = 30.0 }
+        };
+
+            return View(expenditures);
+        }
+    }
+}
---
+++ Models/Expenditure.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Templating_Razor.Models
+{
+    public class Expenditure
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+    }
+}
